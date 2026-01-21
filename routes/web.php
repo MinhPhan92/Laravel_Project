b4ecbe7dd987d2c29b550033fbb7653788caed8a
MinhPhan92 <minhpd92@gmail.com>
2026-01-21 09:14:04 +0700

    Init Laravel project

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('hello');
+// });
+
+// Route::get('/product', function () {
+//     return view('product.index');
+// });
+
+// Route::get('/product/add', function () {
+//     return view('product.add');
+// });
+
+// Route::get('/product/{id}', function ($id) {
+//     return view('product.detail', ['id' => $id]);
+// });
+
+Route::prefix('product')->group(function () {
+    Route::get('/', function () {
+        return view('product.index');
+    });
+    Route::get('/add', function () {
+        return view('product.add');
+    })->name('add');
+    Route::get('/{id}', function ($id) {
+        return view('product.detail', ['id' => $id]);
+    });
+});
+
+Route::fallback(function() {
+    return view('error.404');
+});
